e7d511010ebda84088a272982348d31ce580b3f6
HAMAZ MESKI <88968389+HAMZA7onx@users.noreply.github.com>
2022-10-07 18:50:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+VANTA.BIRDS({
+  el: ".yellow_part",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  backgroundColor: 0x2571cf,
+  color1: 0x4c55a7,
+  backgroundAlpha: 0.0,
+});
